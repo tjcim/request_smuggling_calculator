e937f01112ff2161bc9900079d96bf342e2ba040
Trevor Christiansen <trevor@tjc.im>
2022-09-29 11:25:39 -0600

    removed some unneeded comments

--- app.py
+++ app.py
@@ -21,12 +21,3 @@ def calc_length():
     data = request.json
     cl, te = get_length(data["content"])
     return {"content-length": cl, "transfer-encoding": te}
-
-
-# Show a page like this:
-# Content-Length: 0
-# Transfer-Encoding: 0
-# <input box>
-# have an JS on-change on the input that sends the contents of the input box to an API endpoint
-# The API endpoint calculates the new values and sends back a JSON object
-# The JS script updates the values on the page
